890dac61c2acf1658d7b28f435b4b0db062c8481
Andrew McLees <andrewmclees5@gmail.com>
2018-08-11 12:26:41 -0700

    Add room header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -20,13 +23,15 @@ import { ViewComponent } from './view/view.component';
 import { ChooseComponent } from './choose/choose.component';
 
 import { SharedService } from './shared/shared.service';
+import { RoomHeaderComponent } from './room-header/room-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HostComponent,
     ViewComponent,
-    ChooseComponent
+    ChooseComponent,
+    RoomHeaderComponent
   ],
   imports: [
     AppRoutingModule,
@@ -34,13 +39,16 @@ import { SharedService } from './shared/shared.service';
     BrowserModule,
     FormsModule,
     MatButtonModule,
+    MatDividerModule,
     MatFormFieldModule,
     MatGridListModule,
     MatIconModule,
     MatInputModule,
     MatProgressSpinnerModule,
+    MatSnackBarModule,
     MatStepperModule,
     MatToolbarModule,
+    MatTooltipModule,
     ReactiveFormsModule
   ],
   providers: [SharedService],
---
+++ src/app/room-header/room-header.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { SharedService } from '../shared/shared.service';
+
+@Component({
+  selector: 'room-header',
+  templateUrl: './room-header.component.html',
+  styleUrls: ['./room-header.component.css']
+})
+export class RoomHeaderComponent {
+  showPassword = false;
+
+  constructor(private sharedService: SharedService,
+    private snackBar: MatSnackBar) { }
+
+  copyToClipboard(text: string, snackBarMessage: string) {
+    const textarea = document.createElement('textarea');
+    textarea.value = text;
+    textarea.style.display = 'hidden';
+
+    document.body.appendChild(textarea);
+
+    textarea.focus();
+    textarea.select();
+
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+
+    this.snackBar.open(snackBarMessage);
+  }
+}
