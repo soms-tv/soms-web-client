77a2615b8410466372a4840a788ea4b48d6d3fdc
Andrew McLees <andrewmclees5@gmail.com>
2018-08-17 23:22:10 -0700

    Add Observable WebSocket to SharedService

--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { webSocket } from 'rxjs/webSocket';
+
 @Injectable()
 export class SharedService {
   roomName: string;
   roomPassword: string;
   serverAddress: string;
   socket: any;
+  socketObservable: Observable<any>;
+
+  constructor() {
+    this.socketObservable = webSocket(this.socket);
+  }
+
+  getSocket(): Observable<any> {
+    return this.socketObservable;
+  }
 }
